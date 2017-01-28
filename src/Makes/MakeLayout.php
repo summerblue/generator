1649a98307dd1f02c65d3d2dde644dc9707552b7
Summer <cj@estgroupe.com>
2017-01-28 11:55:32 +0800

    views

--- src/Makes/MakeLayout.php
+++ src/Makes/MakeLayout.php
@@ -38,7 +38,7 @@ class MakeLayout
     protected function start()
     {
         $ui = $this->scaffoldCommandObj->getMeta()['ui'];
-        $this->putViewLayout('Layout', "Stubs/views/$ui/layout.blade.php.stub", 'layout.blade.php');
+        // $this->putViewLayout('Layout', "Stubs/views/$ui/layout.blade.php.stub", 'layout.blade.php');
         $this->putViewLayout('Error', "Stubs/views/$ui/error.blade.php.stub", 'error.blade.php');
     }
 
